05c706cebdddc2cbc629dd3b2e057dc26aaf8786
Seyla CHEAV <scheav@myges.fr>
2022-12-09 16:17:42 +0100

    Ajout de la fonction factorielle dans la branche 'feature'

--- script.py
+++ script.py
@@ -1,1 +1,9 @@
+#Affiche un message
 print("TP2 GIT avec M.ETTAYEB")
+
+#Calcule la factorielle d'un nombre
+def factorielle(n):
+    if n == 0:
+        return 1
+    else:
+        return n  * factorielle(n-1)
